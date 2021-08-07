8f1c882734b2b485d1fcbafcc07159ff080180f5
masacoco <masaonakama0329@gmail.com>
2021-08-07 07:56:32 +0000

    [Finish]3-8 adding validation

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  
+
   def show
     @blog = Blog.find(params[:id])
   end
@@ -13,31 +13,34 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path(blog.id)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path(@blog.id)
+    else
+      render :new
+    end
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-  end  
-  
+  end
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
     redirect_to blogs_path
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
-  end  
-  
-  
+  end
+
+
 end
